56095c4d88c1706b382b09fef98ac6772a70dff5
Faraz Ahmad <farazahmad.email@gmail.com>
2022-09-06 10:36:21 +0530

    Post request to generate fibonacci sequence

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 import { useUser } from "../contexts/userContext";
 
@@ -12,6 +13,15 @@ function Home() {
         if (!user) navigate("/login");
     }, [user, navigate]);
 
+    const [num1, setNum1] = useState("");
+    const [num2, setNum2] = useState("");
+
+    const onSubmitHandler = async (event) => {
+        event.preventDefault();
+        const response = await axios.post("/fibonacci", { num1, num2 });
+        console.log(response);
+    }
+
     const logoutUser = () => {
         localStorage.removeItem("user");
         setUser(JSON.parse(localStorage.getItem("user")));
@@ -19,8 +29,26 @@ function Home() {
 
     return (
         <div>
-            Home Page: Protected
-            <br />
+            <div>
+                <h1>Fibonacci</h1>
+                <form onSubmit={onSubmitHandler}>
+                    <input
+                        type={"text"}
+                        name="num1"
+                        value={num1}
+                        onChange={(event) => setNum1(event.target.value)}
+                        required
+                    />
+                    <input
+                        type={"text"}
+                        name="num2"
+                        value={num2}
+                        onChange={(event) => setNum2(event.target.value)}
+                        required
+                    />
+                    < button type="submit">Generate Sequence</button>
+                </form>
+            </div>
             {
                 user ? <button onClick={logoutUser}>LogOut</button> : null
             }
